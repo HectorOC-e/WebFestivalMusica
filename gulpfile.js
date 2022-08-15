3dff46e3d3a34de159924e9e01ee407b86f98b8d
HectorOC-e <osoriohector89@gmail.com>
2022-08-15 09:32:56 -0600

    Proyecto Finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,28 @@ const { src, dest, watch, parallel } = require( 'gulp' );
 // CSS
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
 const plumber = require( 'gulp-plumber' );
-
+const autoprefixer = require( 'autoprefixer' );
+const cssnano = require( 'cssnano' );
+const postcss = require ( 'gulp-postcss' );
+const sourcemaps = require( 'gulp-sourcemaps' )
 
 // Imágenes
 const cache = require( 'gulp-cache' );
 const imagemin = require( 'gulp-imagemin' );
 const webp = require( 'gulp-webp' );
+const avif = require( 'gulp-avif' );
+
+// Javascript
+const terser = require( 'gulp-terser-js' );
 
 async function css() {
  
   src( 'src/scss/**/*.scss' )  // Identificar el archivo de SASS
+    .pipe( sourcemaps.init() )
     .pipe( plumber() )
     .pipe(sass())   // Compilarlo
+    .pipe( postcss( [autoprefixer(), cssnano()] ) )
+    .pipe( sourcemaps.write( '.' ) )
     .pipe(dest('build/css'));   // Almacenarla
 }
 
@@ -38,11 +48,32 @@ async function versionWebp() {
     .pipe( dest( 'build/img' ) );
 }
 
-async function dev() {
-    watch( 'src/scss/**/*.scss', css );
+async function versionAvif() {
+  const options = {
+    quality : 50
+  };
+
+  src( 'src/img/**/*.{png,jpg}' )
+    .pipe( avif( options ) )
+    .pipe( dest( 'build/img' ) );
 }
 
-exports.css = css;
+async function javascript() {
+  src( 'src/js/**/*.js' )
+    .pipe( sourcemaps.init() )
+    .pipe( terser() )
+    .pipe( sourcemaps.write( '.' ) )
+    .pipe( dest('build/js') );
+}
+
+async function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/js/**/*.js', javascript );
+}
+
+exports.css = css; 
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel( imagenes, versionWebp, dev );
+exports.versionAvif = versionAvif;
+exports.dev = parallel( imagenes, versionWebp, versionAvif, javascript, dev );
